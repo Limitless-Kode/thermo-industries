1cd004603eb75677004e4890279aae94978092db
unknown <limitless.claver@gmail.com>
2021-02-26 15:52:32 +0000

    Website made Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,5 @@ const menu = document.querySelector("#menu");
 const links = document.querySelector(".links");
 
 menu.addEventListener("click",()=> links.classList.toggle("show"));
+
+window.addEventListener("scroll", ()=> links.classList.remove("show"));
