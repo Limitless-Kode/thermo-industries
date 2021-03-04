0bd98dba20474b274964d352d542f0199c5a2a77
unknown <limitless.claver@gmail.com>
2021-03-04 16:36:25 +0000

    contact form validations completed

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,22 +1,102 @@
 const contact_form = document.forms.contact_form;
+const status = document.querySelector("#status");
 
-contact_form.addEventListener("submit", (e)=>{
+document.querySelectorAll("input").forEach((field)=>{
+    field.addEventListener("change",()=>{
+        field.style.outlineColor = '#179C6B';
+        if(field.parentElement.lastChild.classList?.contains("error"))
+            field.parentElement.lastChild.remove();
+    });
+});
+
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("change",()=>{
+    textarea.style.outlineColor = '#179C6B';
+    if(textarea.parentElement.lastChild.classList?.contains("error"))
+        textarea.parentElement.lastChild.remove();
+});
+
+
+contact_form.addEventListener("submit", async (e)=>{
     e.preventDefault();
     const {name, email, message, phone} = contact_form;
 
+    if(validate({name, email, message, phone})){
+        status.style.color = 'orange';
+        status.innerText = "Sending Message...";
 
+        var formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+        formData.append('message', message);
+        formData.append('phone', phone);
 
+        const request = await fetch(
+            'assets/scripts/mail.php',
+            { 
+                method: 'POST', 
+                mode: "same-origin",
+                credentials: "same-origin",
+                headers: {
+                "Content-Type": "application/json"
+                },
+                body: JSON.stringify({name, email, message, phone})
+            }
+        );
+
+        console.log(request.text());
+
+        status.style.color = '#179C6B';
+        status.innerText = "Message Sent";
+        contact_form.reset();
+    }
 
 });
 
 
-function showMessage({type, message}){
-    console.log(type, message);
+
+window.addEventListener("error",(evt)=>{
+    const {field, description} = evt.detail;
+    field.focus();
+    field.style.outlineColor = 'red';
+
+
+    const errorElement = document.createElement("div");
+    errorElement.classList.add("error");
+    errorElement.innerText = description;
+
+    field.parentElement.append(errorElement);
+});
+
+
+function validate({name, email, message, phone}) {
+    if(!regex.name.test(name.value)){
+        const event = new CustomEvent("error",{detail: {field: name, description: "Please Enter A Valid Full Name"} });
+        window.dispatchEvent(event);
+        return false;
+    }
+    if(!regex.email.test(email.value)){
+        const event = new CustomEvent("error",{detail: {field: email, description: "Please Enter A Valid Email Address"} });
+        window.dispatchEvent(event);
+        return false;
+    }
+    if(!regex.phone.test(phone.value)){
+        const event = new CustomEvent("error",{detail: {field: phone, description: "Please Enter A Valid Phone Number"} });
+        window.dispatchEvent(event);
+        return false;
+    }
+    if(!regex.message.test(message.value)){
+        const event = new CustomEvent("error",{detail: {field: message, description: "Please Enter A Valid Message"} });
+        window.dispatchEvent(event);
+        return false;
+    }
+    return true;
 }
+
 
 const regex = {
     'name' : /^[A-z]{2,20}\s[A-z]{2,20}(\s[A-z]{2,20})?$/i,
     'email' : /^[\w\.]{2,}@[A-z]{2,}\.[A-z]{2,}(\.[A-z]{2,})?$/i,
     'phone': /^(\+[0-9]{3,4})?[0-9]{9,10}$/,
-    'message': /^[]$/
+    'message': /^[\w\.\s]{2,}$/
 }
--- assets/scripts/mail.php
+++ assets/scripts/mail.php
@@ -2,11 +2,29 @@
 $to_email = "rockpeter734@gmail.com";
 $subject = "Simple Email Test via PHP";
 $body = "Hi,nn This is test email send by PHP Script";
-$headers = "From: sender\'s email";
+$headers = "From: limitless@gmail.com";
+
  
 if (mail($to_email, $subject, $body)) {
     echo "Email successfully sent to $to_email...";
 } else {
     echo "Email sending failed...";
 }
+
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+
+if ($contentType === "application/json") {
+  //Receive the RAW post data.
+  $content = trim(file_get_contents("php://input"));
+
+  $decoded = json_decode($content, true);
+
+  //If json_decode failed, the JSON is invalid.
+  if(! is_array($decoded)) {
+
+  } else {
+    // Send error back to user.
+  }
+}
+
 ?>
